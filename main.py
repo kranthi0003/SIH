e855177c93ec6e21fc24f51647a3caf21a4fdd68
kamesh1106 <akasapu.kamesh11@gmail.com>
2020-01-05 12:22:56 +0530

    ElasticSearch

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import urllib.request
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, session, url_for
 from werkzeug.utils import secure_filename
-from pdftotext import text_Convertor
+from forms import UploadForm, EntryForm, SearchForm, QuesSearchForm
+import PyPDF2
+from myclass import es
+import elasticsearch
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
@@ -10,42 +13,161 @@ app = Flask(__name__)
 
 app.secret_key = "SIH"
 
-UPLOAD_FOLDER ='/home/kranthi-kiran/Desktop/app/uploads'
-##if using windows change the above path to store the uploaded files 
+UPLOAD_FOLDER ='G:/SIH/Project/SIH/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
-# max size of 200MB
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def convertor():
-	convert = text_Convertor()
+def convertor(filename):
+	pdfFileObj = open('G:/SIH/Project/SIH/uploads/'+filename, 'rb')
+	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+	print("Number of pages:-"+str(pdfReader.numPages))
+	num = pdfReader.numPages
+	i =0
+	file_res = open('G:/SIH/Project/SIH/converts/'+filename.split('.')[0]+'.txt','w',encoding='UTF-8')
+	while(i<num):
+		pageObj = pdfReader.getPage(i)
+		text=pageObj.extractText()
+		#print(text)
+		file_res.write('\n\nPage: '+str(i+1)+'\n\n'+text)
+		i=i+1
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/start', methods = ['GET', 'POST'])
 def upload_form():
-	return render_template('index.html')
+	form1 = UploadForm()
+	form2 = QuesSearchForm()
+	res = []
+	es = elasticsearch.Elasticsearch()
+	if(form1.validate_on_submit() and ('submit' in request.form)):
+		if(request.form['submit'] == 'Submit'):
+			print('Hi1')
+			if 'file' not in request.files:
+				flash('No file part')
+				return redirect(request.url)
+			file = request.files['file']
+			print(file.filename)
+			if file.filename == '':
+				flash('No file selected for uploading')
+				return redirect(request.url)
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+				flash('File successfully uploaded')
+				convertor(filename)
+				print(filename.split('.')[0])
+				#session['filename'] = filename.split('.')[0]
+				filename = filename.split('.')[0]
+				author = 'myind4'
+				book = open('G:/SIH/Project/SIH/converts/'+filename+'.txt')
+				lineNum = 0
+				txtNum = 0
+				try:
+					for lineText in book:
+					    lineNum += 1
+					    if len(lineText) > 0:
+					        txtNum += 1
+					        es.index(index=author, id=txtNum, body = {'lineNum': lineNum,'text': lineText})
+				except UnicodeDecodeError as e:
+					print("Decode error at: " + str(lineNum) + ':' + str(txtNum))
+					print(e)
+					book.close()
+				print(es.get(index=author, id=txtNum))
 
-@app.route('/', methods=['POST'])
-def upload_file():
-	if request.method == 'POST':
-        # check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			convertor()
-			return redirect('/')
+			else:
+				flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+				return redirect(request.url)
+
+	if(form2.validate_on_submit() and ('submit' in request.form)):
+		if(request.form['submit'] == 'Search'):
+			print('HI')
+			'''print('HI')
+			session['index'] = form2.index.data
+			session['query'] = form2.search.data
+			print(form2.index.data)
+			print(form2.search.data)
+			return redirect(url_for('search'))'''
+
+			author = form2.index.data
+			query = form2.search.data
+			print(form2.index.data)
+			print(form2.search.data)
+			numResults = 10
+
+			results = es.search(
+				index=author,
+				body={
+				    "size": numResults,
+				    "query": {"match": {"text": {"query": query}}}})
+			print('befRes')
+			print(results)
+
+			hitCount = results['hits']['total']['value']
+			print(hitCount)
+			if hitCount > 0:
+				if hitCount is 1:
+				    print(str(hitCount) + ' result')
+				else:
+				    print(str(hitCount) + ' results')
+				    
+				for hit in results['hits']['hits']:
+				    text = hit['_source']['text']
+				    lineNum = hit['_source']['lineNum']
+				    score = hit['_score']
+				    title = hit['_type']
+				    if lineNum > 1:
+				        previousLine = es.get(index=author,id=lineNum-1)
+				    #nextLine = es.get(index=author, id=lineNum+1)
+				    res.append(title + ': ' + str(lineNum) + ' (' + str(score) + '): ')
+				    res.append(previousLine['_source']['text'] + text + nextLine['_source']['text'])
+				print(res)
+			else:
+				print('No results')
+
+			#return render_template('start.html', res=res)
+	res = res	
+	return render_template('start.html', form1 = form1, form2 = form2, res = res)
+
+
+'''@app.route('/search', methods = ['GET', 'POST'])
+def search():
+	numResults = 10
+	es = elasticsearch.Elasticsearch()
+
+	results = es.search(
+		index=author,
+		body={
+		    "size": numResults,
+		    "query": {"match": {"text": {"query": query}}}})
+
+	print(results)
+
+	hitCount = results['hits']['total']['value']
+	print(hitCount)
+	if hitCount > 0:
+		if hitCount is 1:
+		    print(str(hitCount) + ' result')
 		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-			return redirect(request.url)
+		    print(str(hitCount) + ' results')
+		    
+		for hit in results['hits']['hits']:
+		    text = hit['_source']['text']
+		    lineNum = hit['_source']['lineNum']
+		    score = hit['_score']
+		    title = hit['_type']
+		    if lineNum > 1:
+		        previousLine = es.get(index=author,id=lineNum-1)
+		    nextLine = es.get(index=author, id=lineNum+1)
+		    res.append(title + ': ' + str(lineNum) + ' (' + str(score) + '): ')
+		    res.append(previousLine['_source']['text'] + text + nextLine['_source']['text'])
+	else:
+		print('No results')
+
+	#print(form)
+
+	return render_template('search.html', res=res)'''
 
 if __name__ == "__main__":
     app.run(debug=True)
--- pdftotext.py
+++ pdftotext.py
@@ -1,15 +1,16 @@
 import PyPDF2
 
 class text_Convertor():
-    pdfFileObj = open('/home/kranthi-kiran/Desktop/app/uploads/sample.pdf', 'rb')
+    print('Hi1')
+    pdfFileObj = open('G:/SIH/Project/SIH/uploads/sample.pdf', 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     print("Number of pages:-"+str(pdfReader.numPages))
     num = pdfReader.numPages
     i =0
-    file_res = open('/home/kranthi-kiran/Desktop/app/text_file/text.txt','w',encoding='UTF-8')
+    file_res = open('G:/SIH/Project/SIH/converts/sample1.txt','w',encoding='UTF-8')
     while(i<num):
         pageObj = pdfReader.getPage(i)
         text=pageObj.extractText()
-        #print(text)
+        print(text)
         file_res.write('\n\nPage: '+str(i+1)+'\n\n'+text)
         i=i+1
